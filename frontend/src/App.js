8770e57dee5c9be4e1629de92c6f8f36e3eb09bb
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-07-21 00:09:17 +0300

    step 11 @rollbar/react added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 // import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { Provider } from 'react-redux';
 
 import './App.css';
@@ -12,21 +13,36 @@ import SignupPage from './components/SignupPage';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import store from './slices/index.js';
 
+const rollbarConfig = {
+  accessToken: 'a1135da28f624efd87a3ea30b66f86ba',
+  environment: 'dev',
+};
+
+// function TestError() {
+//   const a = null;
+//   return a.hello();
+// }
+
 const App = () => (
-  <AuthProvider>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<RequireAuth><MainPage /></RequireAuth>} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="signup" element={<SignupPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </AuthProvider>
+  <RollbarProvider config={rollbarConfig}>
+    <ErrorBoundary>
+      {/* <TestError /> */}
+      <AuthProvider>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route path="/" element={<RequireAuth><MainPage /></RequireAuth>} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="signup" element={<SignupPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </Provider>
+      </AuthProvider>
+    </ErrorBoundary>
+  </RollbarProvider>
 );
 
 export default App;
