da1f5dafe03a21b14782036095eea784700a67c4
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-07-20 23:43:29 +0300

    step 10 - leo-profanity added

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,8 +1,26 @@
-const Message = ({ message }) => (
-  <div className="text-break mb-2">
-    <b>{message.username}</b>
-    {`: ${message.body}`}
-  </div>
-);
+import filter from 'leo-profanity';
+
+const Message = ({ message }) => {
+  // in this state,
+// the "filter" only use "english" dictionary by default
+  filter.list();
+
+  // remove all bad words from the filter
+  // now the filter can't filter anything cause there are no bad words
+  filter.clearList();
+
+  // adding word (from builtin dictionary) into the filter
+  filter.add(filter.getDictionary('en'));
+  filter.add(filter.getDictionary('fr'));
+  filter.add(filter.getDictionary('ru'));
+  // filter.loadDictionary('ru');
+  const text = filter.clean(message.body);
+  return (
+    <div className="text-break mb-2">
+      <b>{message.username}</b>
+      {`: ${text}`}
+    </div>
+  );
+};
 
 export default Message;
