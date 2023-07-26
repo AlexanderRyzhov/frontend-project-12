a273e8412069f35268bbece61c10436425ea3d15
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-07-26 14:42:51 +0300

    Message render fix

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -18,7 +18,8 @@ const Message = ({ message }) => {
   return (
     <div className="text-break mb-2">
       <b>{message.username}</b>
-      {`: ${text}`}
+      {': '}
+      {text}
     </div>
   );
 };
