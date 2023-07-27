c1131c07628275707e45788217caaea1c26d58dc
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-07-27 10:58:12 +0300

    add channel button fix

--- frontend/src/components/Channels.jsx
+++ frontend/src/components/Channels.jsx
@@ -44,6 +44,7 @@ const Channels = () => {
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
+          <span className="visually-hidden">+</span>
         </Button>
       </div>
       <ul id="channels-box" className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
