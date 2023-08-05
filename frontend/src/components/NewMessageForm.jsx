d6d27fe0806892ff15e3890ea675d840e94b9a12
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-08-06 00:01:55 +0300

    NewMessageForm useSocket -> useSocketApi

--- frontend/src/components/NewMessageForm.jsx
+++ frontend/src/components/NewMessageForm.jsx
@@ -3,11 +3,11 @@ import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 
-import { useSocket } from '../contexts/SocketContext';
+import { useSocketApi } from '../contexts/SocketContext';
 
 const NewMessageForm = ({ channelId, username }) => {
   const { t } = useTranslation();
-  const api = useSocket();
+  const api = useSocketApi();
   const [message, setMessage] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const currentChannelId = useSelector((state) => {
