e04ab45e60a2aa07a843f8782ceb7bf1095d48f6
AlexanderRyzhov <58718193+AlexanderRyzhov@users.noreply.github.com>
2023-08-15 08:33:32 +0300

    initLeoProfanity added

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,15 +1,6 @@
 import filter from 'leo-profanity';
 
 const Message = ({ message }) => {
-  filter.list();
-  // remove all bad words from the filter
-  // now the filter can't filter anything cause there are no bad words
-  filter.clearList();
-  // adding word (from builtin dictionary) into the filter
-  filter.add(filter.getDictionary('en'));
-  filter.add(filter.getDictionary('fr'));
-  filter.add(filter.getDictionary('ru'));
-
   const text = filter.clean(message.body);
   return (
     <div className="text-break mb-2">
